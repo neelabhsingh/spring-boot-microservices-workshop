102301cc8752aa5dc85fa9f183bf85883b2c271f
Neelabh Singh <neelabhsingh1986@gmail.com>
2020-02-18 17:10:52 +0530

    First commit.

---
+++ movie-cataloge-service/src/main/java/com/movie/moviecatalogeservice/MovieCatalogeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.movie.moviecatalogeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCatalogeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCatalogeServiceApplication.class, args);
+	}
+
+}
---
+++ movie-cataloge-service/src/test/java/com/movie/moviecatalogeservice/MovieCatalogeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.moviecatalogeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
